af7b6f88e10cdc633a7328480d023389eb7b0a44
João de Souza <joaowsouza1337@gmail.com>
2025-09-04 23:04:46 -0300

    Add initial Fastify server setup and package configuration

---
+++ index.js
@@ -0,0 +1,16 @@
+import Fastify from "fastify"
+
+const fastify = Fastify({
+    logger: true
+});
+
+fastify.get('/', function (req, res) {
+    res.send({ message: 'Brasil' });
+});
+
+fastify.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+});
