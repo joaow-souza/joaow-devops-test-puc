b29aca3b322f4cf3cb5419ead9d71b4cdb767155
João de Souza <joaowsouza1337@gmail.com>
2025-09-04 23:32:30 -0300

    Refactor sum function and update tests to use operations module

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,9 @@
-const sum = require('../sum.js');
+const { sum, sub } = require('../operations.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('subs 3 - 1 to equal 2', () => {
+  expect(sub(3, 1)).toBe(2);
 });
