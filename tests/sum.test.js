f885da4c94bd58fba875347a146ea3da99c98c1f
João de Souza <joaowsouza1337@gmail.com>
2025-09-04 23:58:40 -0300

    Add tests for multiply and divide functions in sum.test.js

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,4 +1,4 @@
-const { sum, sub } = require('../operations.js');
+const { sum, sub, multiply, divide } = require('../operations.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -6,4 +6,12 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('subs 3 - 1 to equal 2', () => {
   expect(sub(3, 1)).toBe(2);
+});
+
+test('multiply 5 to 2', () => {
+  expect(multiply(5, 2)).toBe(10);
+});
+
+test('divide 5 to 2', () => {
+  expect(divide(5, 2)).toBe(2.5);
 });
